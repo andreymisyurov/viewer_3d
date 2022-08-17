0a61d219d4d99e90a5e3d7914ac181ce3cf959c0
ANDREY MISYUROV <andrey.misyrov@gmail.com>
2022-08-18 02:10:49 +0700

    make triangle like modern people

--- widget.cpp
+++ widget.cpp
@@ -22,12 +22,13 @@ void Widget::resizeGL(int w, int h) {
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-//    glOrtho(-1,1,-1,1,1,2);
+    //    glOrtho(-1,1,-1,1,1,2);
     glFrustum(-1,1,-1,1,1,2);
 
 }
 
-//float arr[] = {0,0, -1.5, 1,0, -1.5, 0,1, -1.5};
+// массив с вершинами
+float arr[] = {0,0, -1.5, 1,0, -1.5, 0,1, -1.5};
 
 void Widget::paintGL() {
     glClearColor(0, 1, 1, 0);
@@ -35,15 +36,25 @@ void Widget::paintGL() {
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-//    glRotatef(90, 0, 0, 1);
     glTranslated(0, 0, z);
-    glBegin(GL_TRIANGLES);
-    glColor3d(1,0,0);
 
-    glVertex3d(0,0, -1.5);
-    glVertex3d(1,0, -1.5);
-    glVertex3d(0,1, -1.5);
-    glEnd();
+    //вызываем функцию вертекс поинтер
+    // сколько переменных на одну вершину
+    // тип
+    // смещение
+    // адресс массива
+    glVertexPointer(3, GL_FLOAT, 0, &arr);
+
+    // разрешаем использовать вершинный буфер
+    glEnableClientState(GL_VERTEX_ARRAY);
+    //  добавляем цвет треугольника
+    glColor3b(1,0,0);
+    // рисуем треугольники, первый эл = 0, всего вершин 3
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // запрещаем использовать вершинный буфер
+    glDisableClientState(GL_VERTEX_ARRAY);
+
 }
 
 void Widget::changeZ(){
